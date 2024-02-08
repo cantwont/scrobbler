1675886bdb2d9b6449f3bba7782ac23fbe69e0af
x1234 <149845660+cantwont@users.noreply.github.com>
2024-02-07 22:30:31 -0800

    updated for discord webhook support

--- functions/scrobble.py
+++ functions/scrobble.py
@@ -1,9 +1,14 @@
 from functions.misc import split_artist_track
 from functions.network import network
-from functions.config import delay
-import time, datetime, json
+from functions.config import delay, webhook
+from discord_webhook import DiscordWebhook
+import time, datetime, json, random
 
 SCROBBLES = 1
+START_TIME = time.time()
+
+notification = DiscordWebhook(url=webhook, content=f'Started scrobbling @ <t:{str(int(START_TIME))}:T>')
+notification.execute()
 
 def scrobble_(track):
     global SCROBBLES
@@ -20,13 +25,23 @@ def scrobble(*playlist_names, debug=False):
         if isinstance(playlist_name, list):
             for p_name in playlist_name:
                 scrobble(p_name, debug=debug)
-                time.sleep(delay)
+                time.sleep(delay + random.randrange(1, 3))
         elif playlist_name in playlists:
             songs = playlists[playlist_name]
             for song in songs:
+                now = datetime.datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+
                 if debug:
-                    print(f'Scrobbling {song} | {str(SCROBBLES)} scrobbles this session')
+                    print(f'Scrobbling {song} | {str(SCROBBLES)} scrobbles this session ({current_time})')
+                if webhook != False:
+                    if SCROBBLES == 1 and webhook != False:
+                        notification.content = f'1 scrobble this session\nLast scrobbled @ <t:{str(int(time.time()))}:T>'
+                        notification.edit()
+                    elif SCROBBLES > 1 and webhook != False:
+                        notification.content = f'{str(SCROBBLES)} scrobbles this session\nLast scrobbled @ <t:{str(int(time.time()))}:T>\nLast song: {song}'
+                        notification.edit()
                 scrobble_(str(song))
-                time.sleep(delay)
+                time.sleep(delay + random.randrange(1, 3))
         else:
             print("Playlist '{}' not found.".format(playlist_name))
